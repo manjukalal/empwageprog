dca724c77e16af2199c14a4bca8c0a565e27f93e
manjukalal <manjukalal1496@gmail.com>
2022-05-06 13:35:04 +0530

    emp wage per month

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -1,17 +1,24 @@
 #!/bin/bash -x
+
 isparttime=1;
 isfulltime=2;
 emprateperhr=20;
+workingdays=20;
+totalsalary=0;
 
-empcheck=$(( RANDOM%3 ));
-
-case $empcheck in
-$isfulltime)
-	emphrs=8;;
-$isparttime)
-	emphrs=4;;
-	*)
-	emphrs=0;;
+for (( day=1; day<=$workingdays; day++ ))
+do
+        empcheck=$(( RANDOM%3 ))
+	case $empcheck in
+		$isfulltime)
+			emphrs=8;;
+		$isparttime)
+			emphrs=4;;
+		*)
+			emphrs=0;;
 esac
 salary=$(($emphrs * $emprateperhr));
-echo "$salary"
+totalsalary=$(($totalsalary + $salary));
+done
+echo "emp sal  is $totalsalary"
+
