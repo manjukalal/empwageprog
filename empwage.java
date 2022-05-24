d81b8024682d4915cde143c6ca502c57c06b4f56
manjukalal <manjukalal1496@gmail.com>
2022-05-24 20:59:19 +0530

    empwage

---
+++ empwage.java
@@ -0,0 +1,28 @@
+
+public class empwageuc4 {
+
+
+    public static void main(String[] args) {
+        int rateperhrs= 20;
+        int fulltime= 1;
+        int parttime= 2;
+        int empwage= 0;
+        int emphr = 0;
+        int empcheck = (int)Math.floor(Math.random()*10)%3;
+        switch (empcheck)
+        {
+            case 1:
+                emphr = 4;
+                break;
+
+            case 2:
+                emphr = 8;
+                break;
+
+            default:
+                emphr =0;
+                break;
+        }
+        empwage =  ( emphr * rateperhrs );
+        System.out.println ( empwage );
+    }
